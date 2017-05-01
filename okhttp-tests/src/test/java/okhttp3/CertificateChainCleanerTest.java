square/okhttp

Fix Connection sharing across equivalent OkHttpClients
Fixes https://github.com/square/okhttp/issues/2757

The OkHttpClient(Builder) constructor and both overloads of
OkHttpClient.Builder.sslSocketFactory() set a new CertificateChainCleaner
instance on the OkHttpClient.

Before this CL, CertificateChainCleaner, CertificatePinner and
TrustRootIndex used object rather than value identity semantics. This
caused Address.equals() to not recognize when addresses were equivalent,
which broke connection pooling across OkHttpClient instances.

This CL changes CertificateChainClaner, CertificatePinner and
TrustRootIndex to use value equality by overriding equals() and hashCode().

CertificatePinner.pins and BasicTrustRootIndex.subjectToCaCerts's values
have changed from List to Set because their order shouldn't matter.
This CL uses LinkedHashSet, although HashSet should also work.

okhttp-tests/src/test/java/okhttp3/CertificateChainCleanerTest.java
@@ -21,6 +21,10 @@ import java.security.cert.X509Certificate;
 import java.util.ArrayList;
 import java.util.List;
 import javax.net.ssl.SSLPeerUnverifiedException;
+import javax.net.ssl.SSLSocketFactory;
+import javax.net.ssl.X509TrustManager;
+
+import okhttp3.internal.platform.Platform;
 import okhttp3.internal.tls.CertificateChainCleaner;
 import okhttp3.internal.tls.HeldCertificate;
 import org.junit.Test;
@@ -29,6 +33,27 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
 
 public final class CertificateChainCleanerTest {
+  @Test public void equals_fromCertificate() throws Exception {
+    HeldCertificate rootA = new HeldCertificate.Builder()
+            .serialNumber("1")
+            .build();
+    HeldCertificate rootB = new HeldCertificate.Builder()
+            .serialNumber("2")
+            .build();
+    assertEquals(
+            CertificateChainCleaner.get(rootA.certificate, rootB.certificate),
+            CertificateChainCleaner.get(rootB.certificate, rootA.certificate));
+  }
+
+  @Test public void equals_fromTrustManager() throws Exception {
+    Platform platform = Platform.get();
+    X509TrustManager x509TrustManager = platform.trustManager(
+            (SSLSocketFactory) SSLSocketFactory.getDefault());
+    assertEquals(
+            CertificateChainCleaner.get(x509TrustManager),
+            CertificateChainCleaner.get(x509TrustManager));
+  }
+
   @Test public void normalizeSingleSelfSignedCertificate() throws Exception {
     HeldCertificate root = new HeldCertificate.Builder()
         .serialNumber("1")

asf-stripe/okhttp-code2040

okhttp-tests/src/test/java/okhttp3/CertificateChainCleanerTest.java
@@ -21,6 +21,9 @@ import java.security.cert.X509Certificate;
 import java.util.ArrayList;
 import java.util.List;
 import javax.net.ssl.SSLPeerUnverifiedException;
+import javax.net.ssl.SSLSocketFactory;
+import javax.net.ssl.X509TrustManager;
+import okhttp3.internal.platform.Platform;
 import okhttp3.internal.tls.CertificateChainCleaner;
 import okhttp3.internal.tls.HeldCertificate;
 import org.junit.Test;
@@ -29,6 +32,27 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
 
 public final class CertificateChainCleanerTest {
+  @Test public void equalsFromCertificate() throws Exception {
+    HeldCertificate rootA = new HeldCertificate.Builder()
+        .serialNumber("1")
+        .build();
+    HeldCertificate rootB = new HeldCertificate.Builder()
+        .serialNumber("2")
+        .build();
+    assertEquals(
+        CertificateChainCleaner.get(rootA.certificate, rootB.certificate),
+        CertificateChainCleaner.get(rootB.certificate, rootA.certificate));
+  }
+
+  @Test public void equalsFromTrustManager() throws Exception {
+    Platform platform = Platform.get();
+    X509TrustManager x509TrustManager = platform.trustManager(
+        (SSLSocketFactory) SSLSocketFactory.getDefault());
+    assertEquals(
+        CertificateChainCleaner.get(x509TrustManager),
+        CertificateChainCleaner.get(x509TrustManager));
+  }
+
   @Test public void normalizeSingleSelfSignedCertificate() throws Exception {
     HeldCertificate root = new HeldCertificate.Builder()
         .serialNumber("1")